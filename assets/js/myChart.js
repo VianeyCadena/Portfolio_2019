77aef54254613dfb311a233c08c648b48399d2b1
Vianey del Carmen <Vianey@AIR19087.local>
2019-10-22 19:16:41 -0500

    add skills, welcome

--- assets/js/myChart.js
+++ assets/js/myChart.js
@@ -1,4 +1,4 @@
-function floatPercent(value){
+/* function floatPercent(value){
     return "% "+(value).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
@@ -37,4 +37,55 @@ $("#renderBtn").click(
         labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
         renderChart(data, labels);
     }
-);
+); */
+
+$(document).ready(function(){
+    data = [80, 80, 60, 70, 60, 50, 80, 75, 65, 60, 50, 55, 60, 55, 70, 65, 100];
+    labels = ["Photoshop", "Illustrator", "InDesign", "After Effects", "Premiere Pro", "Audition", "HTML", "CSS", "Bootstrap", "JS", "jQuery", "ReactJS", "NodeJS", "Express", "MySQL", "Firebase"];
+    renderChart(data,labels);
+});
+
+function floatPercent(value){
+    return (value).toFixed(0).replace(/\d(?=(\d{3})+\.)/g, '$&,') + "% ";
+}
+
+
+function renderChart(data, labels){
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+    var purple_orange_gradient = ctx.createLinearGradient(0, 0, 0, 400);
+        purple_orange_gradient.addColorStop(0, '#7c00ff');
+        purple_orange_gradient.addColorStop(1, '#00f9ff');
+
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: labels,
+        datasets: [{
+            label: "What I Know",
+            data: data,
+            backgroundColor: purple_orange_gradient,
+        }]
+    },
+    options:{
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    callback: function(value, index, values) {
+                        return floatPercent(value);
+                    }
+                }
+            }]
+        }
+    }
+
+});
+}
+
+
